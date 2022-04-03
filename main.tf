2ed6a8a391485f204bb3cf17016f69a473987313
Ronen Cohen <ronenc@gmail.com>
2022-04-03 18:59:24 +0300

    wip

--- main.tf
+++ main.tf
@@ -2,5 +2,5 @@ variable "file_name" {}
 
 resource "local_file" "foo" {
     content  = "foo!"
-    filename = "/home/ronen/tmp/${var.file_name}"
+    filename = "${path.module}/${var.file_name}"
 }
